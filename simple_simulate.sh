ec64a32f45785d22d470d8353337d368691c2494
Jordan Sun <zhuoran.sun@wustl.edu>
2023-05-10 09:15:26 -0500

    Index to param script and modified the lsb script.

--- simple_simulate.sh
+++ simple_simulate.sh
@@ -3,63 +3,10 @@ if [[ -z $1 ]];
 then 
     echo "usage: $0 <index>"
 else
-    echo "Index $1"
-    python3 script.py simple proximity_y $1
-    python3 script.py simple proximity_x $1
-    python3 script.py simple proximity_matrix $1
-    python3 script.py simple simple_random $1 0
-    python3 script.py simple simple_random $1 1
-    python3 script.py simple simple_random $1 2
-    python3 script.py simple simple_random $1 3
-    python3 script.py simple simple_random $1 4
-    python3 script.py simple simple_random $1 5
-    python3 script.py simple simple_random $1 6
-    python3 script.py simple simple_random $1 7
-    python3 script.py simple simple_random $1 8
-    python3 script.py simple simple_random $1 9
-    python3 script.py simple balanced_random $1 0
-    python3 script.py simple balanced_random $1 1
-    python3 script.py simple balanced_random $1 2
-    python3 script.py simple balanced_random $1 3
-    python3 script.py simple balanced_random $1 4
-    python3 script.py simple balanced_random $1 5
-    python3 script.py simple balanced_random $1 6
-    python3 script.py simple balanced_random $1 7
-    python3 script.py simple balanced_random $1 8
-    python3 script.py simple balanced_random $1 9
-    python3 script.py simulate bound $1
-    python3 script.py simulate proximity_y $1
-    python3 script.py simulate proximity_x $1
-    python3 script.py simulate proximity_matrix $1
-    python3 script.py simulate simple_random $1 0
-    python3 script.py simulate simple_random $1 1
-    python3 script.py simulate simple_random $1 2
-    python3 script.py simulate simple_random $1 3
-    python3 script.py simulate simple_random $1 4
-    python3 script.py simulate simple_random $1 5
-    python3 script.py simulate simple_random $1 6
-    python3 script.py simulate simple_random $1 7
-    python3 script.py simulate simple_random $1 8
-    python3 script.py simulate simple_random $1 9
-    python3 script.py simulate balanced_random $1 0
-    python3 script.py simulate balanced_random $1 1
-    python3 script.py simulate balanced_random $1 2
-    python3 script.py simulate balanced_random $1 3
-    python3 script.py simulate balanced_random $1 4
-    python3 script.py simulate balanced_random $1 5
-    python3 script.py simulate balanced_random $1 6
-    python3 script.py simulate balanced_random $1 7
-    python3 script.py simulate balanced_random $1 8
-    python3 script.py simulate balanced_random $1 9
-    python3 script.py simulate lp_max $index
-    python3 script.py simulate lp_random $index 0
-    python3 script.py simulate lp_random $index 1
-    python3 script.py simulate lp_random $index 2
-    python3 script.py simulate lp_random $index 3
-    python3 script.py simulate lp_random $index 4
-    python3 script.py simulate lp_random $index 5
-    python3 script.py simulate lp_random $index 6
-    python3 script.py simulate lp_random $index 7
-    python3 script.py simulate lp_random $index 8
-    python3 script.py simulate lp_random $index 9
+    for offset in {1..20}
+    do
+        let index=$(($offset + $1))
+        echo "Simulating $index."
+        python3 script.py alt_simulate greedy $index -1 2 3
+    done
 fi
