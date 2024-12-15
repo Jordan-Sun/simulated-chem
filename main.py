a346439325b53ffe0921c6efbbaf3a4b278d4edf
Jor <sk.jordan@foxmail.com>
2024-12-15 22:35:55 +0000

    Fix for specific intervals.

--- main.py
+++ main.py
@@ -16,16 +16,20 @@ def process_interval(interval: int, root: str, bias: float = 0.0) -> Assignment:
         with contextlib.redirect_stdout(f):
             # Ask MIQCP to redirect output to python and let us redirect it
             assignment = dynamic_constrained.MIQCP(
-                workload, original_assignment, interval, 1, f"{root}/solutions/{interval}.csv", True, bias)
+                workload, original_assignment, interval, 1, f"{root}/solutions/{interval}.txt", True, bias)
     return assignment
 
 # Read res and procs from arguments
-if len(sys.argv) != 4:
-    print("Usage: python main.py <res> <procs> <bias>")
+if len(sys.argv) < 4:
+    print("Usage: python main.py <res> <procs> <bias> [interval]")
     sys.exit(1)
 res = int(sys.argv[1])
 procs = int(sys.argv[2])
 bias = float(sys.argv[3])
+if len(sys.argv) == 5:
+    interval = int(sys.argv[4])
+else:
+    interval = None
 
 # Read workload file
 workload = Workload.read_csv(f"test/workloads/c{res}.csv")
@@ -42,8 +46,15 @@ else:
 
 os.makedirs(f"{root}/logs", exist_ok=True)
 os.makedirs(f"{root}/solutions", exist_ok=True)
-with multiprocessing.Pool(processes=os.cpu_count()) as pool:
-    assignments = pool.map(partial(process_interval, root=root, bias=bias), range(workload.intervals))
-# Concatenate assignments and write to file
-assignment = Assignment.concatenate(assignments)
-assignment.write_csv(f"{root}/assignment.csv")
+if interval is None:
+    # Run all intervals in parallel
+    with multiprocessing.Pool(processes=os.cpu_count()) as pool:
+        assignments = pool.map(partial(process_interval, root=root, bias=bias), range(workload.intervals), chunksize=1)
+    # Concatenate assignments and write to file
+    assignment = Assignment.concatenate(assignments)
+    assignment.write_csv(f"{root}/assignment.csv")
+else:
+    # Fix specific interval and ignore the assignment output
+    # Output directly to stdout
+    _ = dynamic_constrained.MIQCP(
+        workload, original_assignment, interval, os.cpu_count(), f"{root}/solutions/{interval}.txt", False, bias)
